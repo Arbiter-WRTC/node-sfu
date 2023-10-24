6c7183787a4eabfc0838554a7a01092946fc1869
watzmonium <ixisixi@gmail.com>
2023-10-24 18:38:12 -0400

    test: added tests for Consumer class
    note that it does not seem possible to mock implementation of MediaStreamTrack without significant effort.
    
    Co-authored-by: Tyler Frye <tyler.frye42@gmail.com>
    Co-authored-by: Jay Jayabose <jayabose@gmail.com>

--- src/tests/Consumer.test.js
+++ src/tests/Consumer.test.js
@@ -2,19 +2,130 @@
  * @vitest-environment node
  */
 
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect, vi } from 'vitest';
 import io from 'socket.io-client';
-import Consumer from "../classes/Consumer";
-import EventEmitter from 'events';
+import Consumer from '../classes/Consumer';
+import { RTCPeerConnection, RTCSessionDescription } from 'wrtc';
+import { FakeMediaStreamTrack } from 'fake-mediastreamtrack';
 
-
-describe("Consumer tests", () => {
+describe('Consumer tests', () => {
   // mock the socket connection
 
   // we can try mount(Producer) -> trigger(event) to test event handlers
   it('connects to wss', () => {
     const socket = io('https://localhost:3000');
-    expect(socket).not.toBe(undefined)
+    expect(socket).not.toBe(undefined);
+  });
+
+  it('instantiates a Consumer', () => {
+    const socket = io('https://localhost:3000');
+    const consumer = new Consumer(2, socket, 1, null);
+    expect(consumer.clientId).toBe(1);
+    expect(consumer.remotePeerId).toBe(2);
+    expect(consumer.connection).toBeInstanceOf(RTCPeerConnection);
+    expect(consumer.socket).toBe(socket);
+  });
+
+  it('correctly handles an ice candidate', () => {
+    const socket = io('https://localhost:3000');
+    const spy = vi.spyOn(socket, 'emit');
+    const consumer = new Consumer(2, socket, 1, null);
+    const candidate = {
+      candidate: true,
+      clientId: consumer.clientId,
+      remotePeerId: consumer.remotePeerId,
+    };
+
+    consumer.handleRtcIceCandidate(candidate);
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith(
+      'consumerHandshake',
+      expect.objectContaining(candidate)
+    );
+  });
+
+  it('calls handle ice candidate with an object without a candidate prop', () => {
+    const socket = io('https://localhost:3000');
+    const spy = vi.spyOn(socket, 'emit');
+    const consumer = new Consumer(2, socket, 1, null);
+    const candidate = {
+      clientId: consumer.clientId,
+      remotePeerId: consumer.remotePeerId,
+    };
+    consumer.handleRtcIceCandidate(candidate);
+
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it('makes an offer, sets local description, and emits a handshake', async () => {
+    const socket = io('https://localhost:3000');
+    const spy = vi.spyOn(socket, 'emit');
+    const consumer = new Consumer(2, socket, 1, null);
+    const offerSpy = vi.spyOn(consumer.connection, 'createOffer');
+    const setLocalDescSpy = vi.spyOn(
+      consumer.connection,
+      'setLocalDescription'
+    );
+
+    await consumer.handleRtcConnectionNegotiation();
+
+    expect(offerSpy).toHaveBeenCalled();
+    expect(setLocalDescSpy).toHaveBeenCalled();
+    expect(spy).toBeCalledWith(
+      'consumerHandshake',
+      expect.objectContaining({
+        clientId: consumer.clientId,
+        remotePeerId: consumer.remotePeerId,
+      })
+    );
+  });
+
+  it('adds a track to the RTCPeerConnection', () => {
+    const socket = io('https://localhost:3000');
+    const consumer = new Consumer(2, socket, 1, null);
+    const track = new FakeMediaStreamTrack({ kind: 'audio' });
+
+    expect(() => consumer.addTrack(track)).toThrow(
+      'This is not an instance of MediaStreamTrack'
+    );
+  });
+
+  it('sets remote description when given a response sdp', async() => {
+    const socket = io('https://localhost:3000');
+    const consumer = new Consumer(2, socket, 1, null);
+    const remoteDescSpy = vi.spyOn(consumer.connection, 'setRemoteDescription');
+    const description = new RTCSessionDescription();
+    description.type = 'offer';
+    description.sdp = `v=0\no=- 1234567890 1234567890 IN IP4 127.0.0.1\ns=Test Session\nt=0 0\n`;
+
+    await consumer.handshake(description, undefined)
+
+    expect(remoteDescSpy).toHaveBeenCalled();
   })
 
+  it('correctly handles a handshake with an ice candidate', async() => {
+    const socket = io('https://localhost:3000');
+    const consumer = new Consumer(2, socket, 1, null);
+    const remoteDescSpy = vi.spyOn(consumer.connection, 'setRemoteDescription');
+    const iceCandidateSpy = vi.spyOn(consumer.connection, 'addIceCandidate');
+
+    await consumer.handshake(undefined, {candidate: [true]})
+
+    expect(remoteDescSpy).not.toHaveBeenCalled();
+    expect(iceCandidateSpy).toHaveBeenCalled();
+  });
+
+  it('correctly handles a handshake with an ice candidate with too many elements', async() => {
+    const socket = io('https://localhost:3000');
+    const consumer = new Consumer(2, socket, 1, null);
+    const remoteDescSpy = vi.spyOn(consumer.connection, 'setRemoteDescription');
+    const iceCandidateSpy = vi.spyOn(consumer.connection, 'addIceCandidate');
+
+    expect(() => consumer.handshake(undefined, {foo: 'bar'}).toThrow('TypeError'))
+
+    expect(remoteDescSpy).not.toHaveBeenCalled();
+    expect(iceCandidateSpy).not.toHaveBeenCalled();
+
+  });
 });
--- src/tests/Producer.test.js
+++ src/tests/Producer.test.js
@@ -6,7 +6,8 @@ import { describe, it, expect, vi, mount } from 'vitest';
 import io from 'socket.io-client';
 import Producer from '../classes/Producer';
 import EventEmitter from 'events';
-import { RTCSessionDescription } from 'wrtc';
+import { RTCPeerConnection, RTCSessionDescription } from 'wrtc';
+import {FakeMediaStreamTrack} from 'fake-mediastreamtrack'
 
 describe('Producer tests', () => {
   // we can try mount(Producer) -> trigger(event) to test event handlers
@@ -20,8 +21,8 @@ describe('Producer tests', () => {
     const producer = new Producer(socket, 1, new EventEmitter(), null);
     expect(producer.socket).not.toBe(undefined);
     expect(producer.id).toBe(1);
-    expect(producer.connection).not.toBe(undefined);
-    expect(producer.eventEmitter).not.toBe(undefined);
+    expect(producer.connection).toBeInstanceOf(RTCPeerConnection)
+    expect(producer.eventEmitter).toBeInstanceOf(EventEmitter)
   });
 
   it('calls handle ice candidate with an object with a candidate prop', () => {
@@ -29,7 +30,9 @@ describe('Producer tests', () => {
     const spy = vi.spyOn(socket, 'emit');
     const producer = new Producer(socket, 1, new EventEmitter(), null);
     const candidate = { candidate: true, clientId: producer.id };
+
     producer.handleRtcIceCandidate(candidate);
+
     expect(spy).toHaveBeenCalled();
     expect(spy).toHaveBeenCalledWith(
       'producerHandshake',
@@ -42,7 +45,9 @@ describe('Producer tests', () => {
     const spy = vi.spyOn(socket, 'emit');
     const producer = new Producer(socket, 1, new EventEmitter(), null);
     const candidate = { foo: true, clientId: producer.id };
+
     producer.handleRtcIceCandidate(candidate);
+
     expect(spy).not.toHaveBeenCalled();
   });
 
@@ -50,13 +55,15 @@ describe('Producer tests', () => {
     const socket = io('https://localhost:3000');
     const producer = new Producer(socket, 1, new EventEmitter(), null);
     const spy = vi.spyOn(producer.eventEmitter, 'emit');
-    const track = { track: { kind: 'audio' } };
-    producer.handleRtcPeerTrack(track);
-    expect(producer.mediaTracks['audio']).toBe(track.track);
+    const track = new FakeMediaStreamTrack({ kind: 'audio' });
+    
+    producer.handleRtcPeerTrack({track});
+
+    expect(producer.mediaTracks['audio']).toBe(track);
     expect(spy).toHaveBeenCalled();
     expect(spy).toHaveBeenCalledWith(
       'producerTrack',
-      expect.objectContaining(track)
+      expect.objectContaining({track})
     );
   });
 
@@ -70,7 +77,9 @@ describe('Producer tests', () => {
     const description = new RTCSessionDescription();
     description.type = 'offer';
     description.sdp = `v=0\no=- 1234567890 1234567890 IN IP4 127.0.0.1\ns=Test Session\nt=0 0\n`;
+
     await producer.handshake(description, undefined);
+
     expect(remoteDescSpy).toHaveBeenCalled();
     expect(answerSpy).toHaveBeenCalled();
     expect(localDescSpy).toHaveBeenCalled();
@@ -88,8 +97,12 @@ describe('Producer tests', () => {
     const spy = vi.spyOn(socket, 'emit');
     const producer = new Producer(socket, 1, new EventEmitter(), null);
     const remoteDescSpy = vi.spyOn(producer.connection, 'setRemoteDescription');
-
     producer.isNegotiating = true;
+    const description = new RTCSessionDescription();
+    description.type = 'offer';
+    description.sdp = `v=0\no=- 1234567890 1234567890 IN IP4 127.0.0.1\ns=Test Session\nt=0 0\n`;
+
+    await producer.handshake(description, undefined);
 
     expect(remoteDescSpy).not.toHaveBeenCalled();
     expect(spy).not.toHaveBeenCalled();
@@ -97,24 +110,24 @@ describe('Producer tests', () => {
 
   it('correctly handles a handshake with an ice candidate', async() => {
     const socket = io('https://localhost:3000');
-    const spy = vi.spyOn(socket, 'emit');
     const producer = new Producer(socket, 1, new EventEmitter(), null);
     const remoteDescSpy = vi.spyOn(producer.connection, 'setRemoteDescription');
     const iceCandidateSpy = vi.spyOn(producer.connection, 'addIceCandidate');
 
     await producer.handshake(undefined, {candidate: [true]})
+
     expect(remoteDescSpy).not.toHaveBeenCalled();
     expect(iceCandidateSpy).toHaveBeenCalled();
   });
 
   it('correctly handles a handshake with an ice candidate with too many elements', async() => {
     const socket = io('https://localhost:3000');
-    const spy = vi.spyOn(socket, 'emit');
     const producer = new Producer(socket, 1, new EventEmitter(), null);
     const remoteDescSpy = vi.spyOn(producer.connection, 'setRemoteDescription');
     const iceCandidateSpy = vi.spyOn(producer.connection, 'addIceCandidate');
 
     expect(() => producer.handshake(undefined, {foo: 'bar'}).toThrow('TypeError'))
+
     expect(remoteDescSpy).not.toHaveBeenCalled();
     expect(iceCandidateSpy).not.toHaveBeenCalled();
 
